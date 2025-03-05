c973e008d48ea952d684bdd6b1d233bd5843caf0
Archbaer <140851415+Archbaer@users.noreply.github.com>
2025-03-05 21:00:45 +0000

    initial setup

---
+++ productsAPI/src/test/java/com/totos/productsAPI/ProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totos.productsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
