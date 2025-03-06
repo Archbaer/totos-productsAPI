c61457533db7d65b2f21ecec1b66b66bbb0d4801
archbaer <pereirakebaili@gmail.com>
2025-03-06 17:26:50 +0000

    Products controller working just fine

--- productsAPI/src/main/java/com/totos/productsAPI/config/SecurityConfig.java
+++ productsAPI/src/main/java/com/totos/productsAPI/config/SecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,6 +27,15 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user123")
+                .build();
+            return new InMemoryUserDetailsManager(user);
     }
 
 }
--- productsAPI/src/main/java/com/totos/productsAPI/controllers/ProductController.java
+++ productsAPI/src/main/java/com/totos/productsAPI/controllers/ProductController.java
@@ -1,10 +1,13 @@
 package com.totos.productsAPI.controllers;
 
+import com.totos.productsAPI.models.Products;
 import com.totos.productsAPI.services.ProductsService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
@@ -15,6 +18,37 @@ public class ProductController {
 
     @GetMapping("/test")
     public String testEndpoint() {
-        return "Endpoint working";
+        Products produto = new Products("Bolacha", "Biscoito bom", 7.99, 9);
+        productService.createProduct(produto);
+        return "Sample product created";
     }
+
+    @GetMapping
+    public List<Products> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Products getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+
+    @PostMapping
+    // @PreAuthorize("hasRole('ADMIN') or hasRole('API')")
+    public Products createProduct(@Valid @RequestBody Products product) {
+        return productService.createProduct(product);
+    }
+
+    @PutMapping("/{id}")
+    // @PreAuthorize("hasRole('ADMIN') or hasRole('API')")
+    public Products updateProduct(@PathVariable Long id, @Valid @RequestBody Products product) {
+        return productService.updateProduct(id, product);
+    }
+
+    @DeleteMapping("/{id}")
+    // @PreAuthorize("hasRole('ADMIN') or hasRole('API')")
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+    }
+
 }
