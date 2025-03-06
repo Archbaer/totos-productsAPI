8831178b947ea3c76992e3bcfe00a87cb113ff9f
archbaer <pereirakebaili@gmail.com>
2025-03-06 18:22:50 +0000

    minor corrections

--- productsAPI/src/main/java/com/totos/productsAPI/filter/JwtAuthenticationFilter.java
+++ productsAPI/src/main/java/com/totos/productsAPI/filter/JwtAuthenticationFilter.java
@@ -11,17 +11,19 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.security.PublicKey;
 import java.util.Collections;
 import java.util.List;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private PublicKeyService publicKeyService;
+    private final PublicKeyService publicKeyService;
 
-    public PublicKeyService getPublicKeyService(PublicKeyService publicKeyService) {
+    public JwtAuthenticationFilter(PublicKeyService publicKeyService) {
         this.publicKeyService = publicKeyService;
     }
 
